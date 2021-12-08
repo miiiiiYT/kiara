e12737a78ccd4900152a50677a1f74f9e7990ac2
Sidhak <sidhak381011@gmail.com>
2021-12-08 19:00:20 +0530

    Changed to mongodb

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import aiohttp
 import json
 from datetime import datetime
+from pymongo import MongoClient
 
 timestamp = int(datetime.now().timestamp())
 print(timestamp)
@@ -85,8 +86,14 @@ async def on_ready():
     print("Ready")
     await client.change_presence(status=nextcord.Status.online, activity=nextcord.Game("Default prefix - ?"))
 
-
 # Commands
+'''#Custom messages
+@client.command()
+async def custom_message(message):
+    channel_1 = client.get_channel(894953882424320080)
+    embed=nextcord.Embed(title="Annoucement!", description="Ping - <@&894557281931391047>")
+    embed.add_field(name="Annoucement signed by Metolix", value="The economy system will be shutdown again cause we need to switch to a database from JSON files. Stay tuned here for more info when the economy system is back. You don't need to worry about your balance it will be backed up.")
+    await channel_1.send(embed=embed)'''
 #Seats
 @client.command()
 async def seats(ctx):
@@ -178,406 +185,136 @@ async def avatar(ctx, member: nextcord.Member = None):
         await ctx.reply(member.avatar)
 
 # Economy
-mainshop = [{"name":"Watch","price":100,"description":"Time"},
-            {"name":"Laptop","price":1000,"description":"Work"},
-            {"name":"PC","price":10000,"description":"Gaming"},
-            {"name":"Ferrari","price":99999,"description":"Sports Car"},
-            {"name":"Premium Economy","price":100,"description":"Sit in Premium Economy Class in flights"},
-            {"name":"Bussiness Class","price":150,"description":"Sit in Bussiness Class in flights"},
-            {"name":"First Class","price":250,"description":"Sit in First Class in flights"}]
-
-
-@client.command(aliases=['bal'])
+@client.command(aliases=["bal"])
+@commands.guild_only()
 async def balance(ctx):
-    await open_account(ctx.author)
+	user = ctx.author
+
+	await open_bank(user)
+
+	users = await get_bank_data(user)
+
+	wallet_amt = users[0]
+	bank_amt = users[1]
+
+	net_amt = int(wallet_amt + bank_amt)
+
+	em = nextcord.Embed(
+			title= f"{user.name}'s Balance",
+			description= f"Wallet: {wallet_amt}\nBank: {bank_amt}",
+			color=nextcord.Color(0x00ff00)
+		)
+
+	await ctx.send(embed=em)
+
+
+@client.command(aliases=["with"])
+@commands.guild_only()
+async def withdraw(ctx, *,amount= None):
     user = ctx.author
+    await open_bank(user)
 
-    users = await get_bank_data()
+    users = await get_bank_data(user)
 
-    wallet_amt = users[str(user.id)]["wallet"]
-    bank_amt = users[str(user.id)]["bank"]
+    bank_amt = users[1]
 
-    em = nextcord.Embed(title=f'{ctx.author.name} Balance',color = nextcord.Color.red())
-    em.add_field(name="Wallet Balance", value=wallet_amt)
-    em.add_field(name='Bank Balance',value=bank_amt)
-    await ctx.send(embed= em)
+    if amount.lower() == "all" or amount.lower() == "max":
+        await update_bank(user, +1*bank_amt)
+        await update_bank(user, -1*bank_amt, "bank")
+        await ctx.send(f"{user.mention} you withdrew {bank_amt} in your wallet")
 
-@client.command()
-@commands.cooldown(2,200,commands.BucketType.user)
-async def beg(ctx):
-    await open_account(ctx.author)
+    amount = int(amount)
+
+    if amount > bank_amt:
+        await ctx.send(f"{user.mention} You don't have that enough money!")
+        return
+
+    if amount < 0:
+        await ctx.send(f"{user.mention} enter a valid amount !")
+        return
+
+    await update_bank(user, +1 * amount)
+    await update_bank(user, -1 * amount, "bank")
+
+    await ctx.send(f"{user.mention} you withdrew **{amount}** from your **Bank!**")
+
+
+@client.command(aliases=["dep"])
+@commands.guild_only()
+async def deposit(ctx, *,amount= None):
     user = ctx.author
+    await open_bank(user)
 
-    users = await get_bank_data()
+    users = await get_bank_data(user)
 
-    earnings = random.randrange(52)
+    wallet_amt = users[0]
 
-    await ctx.send(f'{ctx.author.mention} Got {earnings} FinCoins!!')
-
-    users[str(user.id)]["wallet"] += earnings
-
-    with open("mainbank.json",'w') as f:
-        json.dump(users,f)
-
-
-@client.command(aliases=['wd'])
-async def withdraw(ctx,amount = None):
-    await open_account(ctx.author)
-    if amount == None:
-        await ctx.send("Please enter the amount")
-        return
-
-    bal = await update_bank(ctx.author)
+    if amount.lower() == "all" or amount.lower() == "max":
+        await update_bank(user, -1*wallet_amt)
+        await update_bank(user, +1*wallet_amt, "bank")
+        await ctx.send(f"{user.mention} you withdrew {wallet_amt} in your wallet")
 
     amount = int(amount)
 
-    if amount > bal[1]:
-        await ctx.send('You do not have sufficient balance')
+    if amount > wallet_amt:
+        await ctx.send(f"{user.mention} You don't have that enough money!")
         return
+
     if amount < 0:
-        await ctx.send('Amount must be positive!')
+        await ctx.send(f"{user.mention} enter a valid amount !")
         return
 
-    await update_bank(ctx.author,amount)
-    await update_bank(ctx.author,-1*amount,'bank')
-    await ctx.send(f'{ctx.author.mention} You withdrew {amount} FinCoins')
+    await update_bank(user, -1 * amount)
+    await update_bank(user, +1 * amount, "bank")
+
+    await ctx.send(f"{user.mention} you withdrew **{amount}** from your **Bank!**")
+# Bank Funcs
+auth_url = "mongodb+srv://Sidhak_3810:<Sidhak@3810>@main.c546s.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
 
-@client.command(aliases=['dp'])
-async def deposit(ctx,amount = None):
-    await open_account(ctx.author)
-    if amount == None:
-        await ctx.send("Please enter the amount")
-        return
+async def open_bank(user):
+    cluster = MongoClient(auth_url)
+    db = cluster["Cluster0"]
 
-    bal = await update_bank(ctx.author)
-
-    amount = int(amount)
-
-    if amount > bal[0]:
-        await ctx.send('You do not have sufficient balance')
-        return
-    if amount < 0:
-        await ctx.send('Amount must be positive!')
-        return
-
-    await update_bank(ctx.author,-1*amount)
-    await update_bank(ctx.author,amount,'bank')
-    await ctx.send(f'{ctx.author.mention} You deposited {amount} FinCoins')
-
-
-@client.command(aliases=['sm'])
-async def send(ctx,member : nextcord.Member,amount = None):
-    await open_account(ctx.author)
-    await open_account(member)
-    if amount == None:
-        await ctx.send("Please enter the amount")
-        return
-
-    bal = await update_bank(ctx.author)
-    if amount == 'all':
-        amount = bal[0]
-
-    amount = int(amount)
-
-    if amount > bal[0]:
-        await ctx.send('You do not have sufficient balance')
-        return
-    if amount < 0:
-        await ctx.send('Amount must be positive!')
-        return
-
-    await update_bank(ctx.author,-1*amount,'bank')
-    await update_bank(member,amount,'bank')
-    await ctx.send(f'{ctx.author.mention} You gave {member} {amount} FinCoins')
-
-
-@client.command(aliases=['rb'])
-@commands.cooldown(1,200,commands.BucketType.user)
-async def rob(ctx,member : nextcord.Member):
-    await open_account(ctx.author)
-    await open_account(member)
-    bal = await update_bank(member)
-
-
-    if bal[0]<100:
-        await ctx.send('It is useless to rob him... *they are poor*')
-        return
-
-    earning = random.randrange(0,bal[0])
-
-    await update_bank(ctx.author,earning)
-    await update_bank(member,-1*earning)
-    await ctx.send(f'{ctx.author.mention} You robbed {member} and got {earning} FinCoins')
-
-
-@client.command()
-@commands.cooldown(1,200,commands.BucketType.user)
-async def slots(ctx,amount = None):
-    await open_account(ctx.author)
-    if amount == None:
-        await ctx.send("Please enter the amount")
-        return
-
-    bal = await update_bank(ctx.author)
-
-    amount = int(amount)
-
-    if amount > bal[0]:
-        await ctx.send('You do not have sufficient balance')
-        return
-    if amount < 0:
-        await ctx.send('Amount must be positive!')
-        return
-    final = []
-    for i in range(3):
-        a = random.choice(['X','O','Q'])
-
-        final.append(a)
-
-    await ctx.send(str(final))
-
-    if final[0] == final[1] or final[1] == final[2] or final[0] == final[2]:
-        await update_bank(ctx.author,2*amount)
-        await ctx.send(f'You won :) {ctx.author.mention}')
-    else:
-        await update_bank(ctx.author,-1*amount)
-        await ctx.send(f'You lose :( {ctx.author.mention}')
-
-
-@client.command()
-async def shop(ctx):
-    em = nextcord.Embed(title = "Shop", description="**Note** - None of the items work, other than the seats, They profit you in flights. Type `f!seats` for more info.")
-
-    for item in mainshop:
-        name = item["name"]
-        price = item["price"]
-        desc = item["description"]
-        em.add_field(name = name, value = f"${price} | {desc}")
-
-    await ctx.send(embed = em)
-
-
-
-@client.command()
-async def buy(ctx,item,amount = 1):
-    await open_account(ctx.author)
-
-    res = await buy_this(ctx.author,item)
-
-    if not res[0]:
-        if res[1]==1:
-            await ctx.send("That Object isn't there!")
-            return
-        if res[1]==2:
-            await ctx.send(f"You don't have enough money in your wallet to buy {amount} {item}")
-            return
-
-
-    await ctx.send(f"You just bought {amount} {item}")
-
-
-@client.command()
-async def bag(ctx):
-    await open_account(ctx.author)
-    user = ctx.author
-    users = await get_bank_data()
+    cursor = db["main"]
 
     try:
-        bag = users[str(user.id)]["bag"]
+        post = {"_id": user.id, "wallet": 5000, "bank": 0}
+
+        cursor.insert_one(post)
+
     except:
-        bag = []
+        pass
 
 
-    em = nextcord.Embed(title = "Bag")
-    for item in bag:
-        name = item["item"]
-        amount = item["amount"]
+async def get_bank_data(user):
+    cluster = MongoClient(auth_url)
+    db = cluster["Cluster0"]
 
-        em.add_field(name = name, value = amount)    
+    cursor = db["main"]
 
-    await ctx.send(embed = em)
+    user_data = cursor.find({"_id": user.id})
 
+    cols = ["wallet", "bank"]
 
-async def buy_this(user,item_name,amount):
-    item_name = item_name.lower()
-    name_ = None
-    for item in mainshop:
-        name = item["name"].lower()
-        if name == item_name:
-            name_ = name
-            price = item["price"]
-            break
+    data = []
 
-    if name_ == None:
-        return [False,1]
+    for mode in user_data:
+        for col in cols:
+            data1 = mode[str(col)]
 
-    cost = price*amount
+            data.append(data1)
 
-    users = await get_bank_data()
-
-    bal = await update_bank(user)
-
-    if bal[0]<cost:
-        return [False,2]
+    return data
 
 
-    try:
-        index = 0
-        t = None
-        for thing in users[str(user.id)]["bag"]:
-            n = thing["item"]
-            if n == item_name:
-                old_amt = thing["amount"]
-                new_amt = old_amt + amount
-                users[str(user.id)]["bag"][index]["amount"] = new_amt
-                t = 1
-                break
-            index+=1 
-        if t == None:
-            obj = {"item":item_name , "amount" : amount}
-            users[str(user.id)]["bag"].append(obj)
-    except:
-        obj = {"item":item_name , "amount" : amount}
-        users[str(user.id)]["bag"] = [obj]        
+async def update_bank(user, amount=0, mode="wallet"):
+    cluster = MongoClient(auth_url)
+    db = cluster["Cluster0"]
 
-    with open("mainbank.json","w") as f:
-        json.dump(users,f)
+    cursor = db["main"]
 
-    await update_bank(user,cost*-1,"wallet")
-
-    return [True,"Worked"]
-    
-
-@client.command()
-async def sell(ctx,item,amount = 1):
-    await open_account(ctx.author)
-
-    res = await sell_this(ctx.author,item,amount)
-
-    if not res[0]:
-        if res[1]==1:
-            await ctx.send("That Object isn't there!")
-            return
-        if res[1]==2:
-            await ctx.send(f"You don't have {amount} {item} in your bag.")
-            return
-        if res[1]==3:
-            await ctx.send(f"You don't have {item} in your bag.")
-            return
-
-    await ctx.send(f"You just sold {amount} {item}.")
-
-async def sell_this(user,item_name,amount,price = None):
-    item_name = item_name.lower()
-    name_ = None
-    for item in mainshop:
-        name = item["name"].lower()
-        if name == item_name:
-            name_ = name
-            if price==None:
-                price = 0.7* item["price"]
-            break
-
-    if name_ == None:
-        return [False,1]
-
-    cost = price*amount
-
-    users = await get_bank_data()
-
-    bal = await update_bank(user)
-
-
-    try:
-        index = 0
-        t = None
-        for thing in users[str(user.id)]["bag"]:
-            n = thing["item"]
-            if n == item_name:
-                old_amt = thing["amount"]
-                new_amt = old_amt - amount
-                if new_amt < 0:
-                    return [False,2]
-                users[str(user.id)]["bag"][index]["amount"] = new_amt
-                t = 1
-                break
-            index+=1 
-        if t == None:
-            return [False,3]
-    except:
-        return [False,3]    
-
-    with open("mainbank.json","w") as f:
-        json.dump(users,f)
-
-    await update_bank(user,cost,"wallet")
-
-    return [True,"Worked"]
-
-
-@client.command(aliases = ["lb"])
-async def leaderboard(ctx,x = 1):
-    users = await get_bank_data()
-    leader_board = {}
-    total = []
-    for user in users:
-        name = int(user)
-        total_amount = users[user]["wallet"] + users[user]["bank"]
-        leader_board[total_amount] = name
-        total.append(total_amount)
-
-    total = sorted(total,reverse=True)    
-
-    em = nextcord.Embed(title = f"Top {x} Richest People" , description = "This is decided on the basis of raw money in the bank and wallet",color = nextcord.Color(0xfa43ee))
-    index = 1
-    for amt in total:
-        id_ = leader_board[amt]
-        member = client.get_user(id_)
-        name = member.name
-        em.add_field(name = f"{index}. {name}" , value = f"{amt}",  inline = False)
-        if index == x:
-            break
-        else:
-            index += 1
-
-    await ctx.send(embed=em)
-
-
-async def open_account(user):
-
-    users = await get_bank_data()
-
-    if str(user.id) in users:
-        return False
-    else:
-        users[str(user.id)] = {}
-        users[str(user.id)]["wallet"] = 0
-        users[str(user.id)]["bank"] = 0
-
-    with open('mainbank.json','w') as f:
-        json.dump(users,f)
-
-    return True
-
-
-async def get_bank_data():
-    with open('mainbank.json','r') as f:
-        users = json.load(f)
-
-    return users
-
-
-async def update_bank(user,change=0,mode = 'wallet'):
-    users = await get_bank_data()
-
-    users[str(user.id)][mode] += change
-
-    with open('mainbank.json','w') as f:
-        json.dump(users,f)
-    bal = users[str(user.id)]['wallet'],users[str(user.id)]['bank']
-    return bal
-
+    cursor.update_one({"_id": user.id}, {"$inc": {str(mode): amount}})
 # Errors
 @client.event
 async def on_command_error(ctx, error):
